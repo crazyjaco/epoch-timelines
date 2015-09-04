994487cd961a9c67d246a9e4819128d69e55c123
crazyjaco <crazyjac0@gmail.com>
2015-09-03 21:00:42 -0400

    Put timeline preloading into modal. TODO: Make ajax request for paging.

--- interface/timeline-modal.php
+++ interface/timeline-modal.php
@@ -1,7 +1,20 @@
+<?php
+// TODO: Turn this call into an ajax request to lazy load timelines
+$timelines = Class_Timeline_Timelines::get_timeline_listing();
+$odd_count = '';
+?>
 <div id="et-timelines-modal">
 	<div id="inner wrap">
 		<ul id="et-timelines-results">
-			<li>STUFF</li>
+			<?php
+			foreach ($timelines as $key => $value ) {
+				// error_log( 'Key: ' . print_r( $key, true ) );
+				// error_log( 'Value: ' . print_r( $value, true ) );
+				$odd = $odd_count % 2 ? 'odd' : '';
+				echo '<li class="' . $odd . '"data-post-id=' . esc_html( $key ) . '>' . esc_html( $value ) . '</li>';
+				$odd_count += 1;
+			}
+			?>
 		</ul>
 	</div>
-</div> 
+</div>
